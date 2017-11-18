005daf04278697c3ba6069720f270dbbf94a3330
Stephan Preibisch <spreibi@Stephans-MacBook-Pro.local>
2017-11-18 15:37:02 -0800

    tying to wrap micromanager images into imglib2

--- src/main/java/net/preibisch/stitcher/mm/MMTest.java
+++ src/main/java/net/preibisch/stitcher/mm/MMTest.java
@@ -1,5 +1,7 @@
 package net.preibisch.stitcher.mm;
 
+import java.util.Iterator;
+
 import org.json.JSONObject;
 import org.micromanager.MMOptions;
 import org.micromanager.MMStudio;
@@ -9,7 +11,13 @@ import ij.IJ;
 import ij.ImageJ;
 import mmcorej.CMMCore;
 import mmcorej.TaggedImage;
-import mpicbg.spim.io.IOFunctions;
+import net.imglib2.img.basictypeaccess.array.ByteArray;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.img.imageplus.ImagePlusImg;
+import net.imglib2.img.planar.PlanarImg;
+import net.imglib2.img.planar.PlanarImgs;
+import net.imglib2.multithreading.SimpleMultiThreading;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
 import net.preibisch.stitcher.plugin.BigStitcher;
 
 public class MMTest
@@ -36,7 +44,7 @@ public class MMTest
 		
 		try
 		{
-			doAcquisition( mm, core );
+			doAcquisition( mm, core, false );
 		}
 		catch ( Exception e )
 		{
@@ -45,14 +53,15 @@ public class MMTest
 		}
 	}
 	
-	public static void doAcquisition(MMStudio gui, CMMCore mmc) throws Exception
+	public static void doAcquisition( final MMStudio gui, final CMMCore mmc, final boolean fromDisk ) throws Exception
 	{
 		gui.closeAllAcquisitions();
 		gui.clearMessageWindow();
-		
-		// use DCam for now
-		mmc.setProperty( "Core", "Camera", "DCam" );
-		// mmc.setProperty("Core", "Camera", "FakeCamera");
+
+		if ( fromDisk )
+			mmc.setProperty("Core", "Camera", "FakeCamera");
+		else
+			mmc.setProperty( "Core", "Camera", "DCam" );
 
 		int nrFrames = 3;
 		int nrSlices = 60;
@@ -65,9 +74,13 @@ public class MMTest
 
 		for ( int f = 0; f < nrFrames; f++ )
 		{
-			String fakePath = fakeRoot + ( f + 1 ) + fakeFile;
-			gui.message( fakePath );
-			mmc.setProperty( "FakeCamera", "Path mask", fakePath );
+			if ( fromDisk )
+			{
+				String fakePath = fakeRoot + ( f + 1 ) + fakeFile;
+				gui.message( fakePath );
+				mmc.setProperty( "FakeCamera", "Path mask", fakePath );
+			}
+
 			for ( int z = 0; z < nrSlices; z++ )
 			{
 				gui.setStagePosition( z );
@@ -81,11 +94,63 @@ public class MMTest
 			int height = gui.getAcquisitionImageHeight( acqName );
 			int nrBytes = gui.getAcquisitionImageByteDepth( acqName );
 			ImageCache imgCache = gui.getAcquisitionImageCache( acqName );
+
+			System.out.println( "w=" + width );
+			System.out.println( "h=" + height );
+			System.out.println( "bytes=" + nrBytes );
+
+			final long[] dim = new long[]{ width, height, nrSlices };
+
+			if ( nrBytes == 1 )
+			{
+				final PlanarImg< UnsignedByteType, ByteArray > img = new PlanarImg< UnsignedByteType, ByteArray >( dim, new UnsignedByteType().getEntitiesPerPixel() );
+
+				for ( int z = 0; z < nrSlices; z++ )
+				{
+					// channel, slice, frame, position
+					TaggedImage image = imgCache.getImage( 0, 5, f, 0 );
+
+					byte[] pixelsPlane = (byte[])image.pix;
+					JSONObject tags = image.tags; // JSONObject
+
+					img.setPlane( z, new ByteArray( pixelsPlane ) );
+				}
+
+				ImageJFunctions.show( img );
+			}
+			else
+			{
+				
+			}
+
+			/*
 			// channel, slice, frame, position
 			TaggedImage img = imgCache.getImage( 0, 5, f, 0 );
-			
+
+
 			Object pixels = img.pix;
 			JSONObject tags = img.tags; // JSONObject
+
+			if ( pixels instanceof byte[] )
+			{
+				System.out.println( ( (byte[]) pixels ).length );
+			}
+			else if ( pixels instanceof short[] )
+			{
+			}
+			else
+			{
+				throw new RuntimeException( "Don't know how to handle images of pixel type: " + pixels.getClass().getSimpleName() );
+			}
+
+			final Iterator< String > keys = tags.keys();
+			while ( keys.hasNext() )
+			{
+				String key = keys.next();
+				System.out.println( key + ": " + tags.get( key ) );
+			}
+
+			System.out.println( pixels.getClass().getSimpleName() );
 
 			// Note that a listener can be attached to an ImageCache
 			// Note: access to images in 1.4 is through an "Acquisition object".
@@ -96,7 +161,8 @@ public class MMTest
 			// retrieved from a DataProvider)
 			// in 2.0, objects from the class Images contain information about
 			// width, height, etc..
-
+			 */
+			SimpleMultiThreading.threadHaltUnClean();
 		}
 
 		
